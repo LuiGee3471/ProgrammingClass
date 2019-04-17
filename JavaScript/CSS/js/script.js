ea750dd6f8397dcb0ec640894ad0e32bcd693350
LuiGee3471 <yjs3475@gmail.com>
2019-04-17 23:16:40 +0900

    CSS 연습 완성

--- JavaScript/CSS/js/script.js
+++ JavaScript/CSS/js/script.js
@@ -1,23 +1,42 @@
 const homepage = document.querySelector(".homepage");
 const navbar = document.querySelector(".navbar");
-const home = document.querySelector(".home");
-const login = document.querySelector(".login");
-const signup = document.querySelector(".signup");
+const homeBtnList = document.querySelectorAll(".home");
+const loginBtn = document.querySelector(".login");
+const signupBtn = document.querySelector(".signup");
 const invBtn = document.querySelector(".inv");
 const visBtn = document.querySelector(".vis");
+const loginPage = document.querySelector(".login-page");
+const signUpPage = document.querySelector(".signup-page");
+
+const INVISIBLE = "invisible";
+const CURRENT = "current";
 
 function makeInvisible() {
   const current = document.querySelector(".current");
-  current.classList.remove("visible");
-  current.classList.add("invisible");
+  current.classList.add(INVISIBLE);
+  current.classList.remove(CURRENT);
 }
 
-function makeVisible(element) {
-  const current = document.querySelector(".current");
-  current.classList.remove("invisible");
-  current.classList.remove("none");
-  current.classList.add("visible");
+function makeVisible(page) {
+  setTimeout(function() {
+    page.classList.remove(INVISIBLE);
+    page.classList.add(CURRENT);
+  }, 300);
 }
 
-invBtn.addEventListener("click", makeInvisible);
-visBtn.addEventListener("click", makeVisible);
+homeBtnList.forEach(homeBtn => {
+  homeBtn.addEventListener("click", function() {
+    makeInvisible();
+    makeVisible(homepage);
+  });
+});
+
+loginBtn.addEventListener("click", function() {
+  makeInvisible();
+  makeVisible(loginPage);
+});
+
+signupBtn.addEventListener("click", function() {
+  makeInvisible();
+  makeVisible(signUpPage);
+});
